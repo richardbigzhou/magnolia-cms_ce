ekirkilevics/Magnolia

commit 20cdbbff969bd9ad16387bf7f9b0eeb933cbfa5e

SCRUM-471 : added getContentMap to the RenderingModel & impl
git-svn-id: file:///home/migrator/subversion-migration/svn-repo-copy/community/magnolia/trunk@50243 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

magnolia-rendering/src/main/java/info/magnolia/rendering/model/RenderingModel.java
@@ -35,6 +35,7 @@ package info.magnolia.rendering.model;
 
 import javax.jcr.Node;
 
+import info.magnolia.jcr.util.ContentMap;
 import info.magnolia.rendering.template.RenderableDefinition;
 
 
@@ -83,6 +84,12 @@ public interface RenderingModel <RD extends RenderableDefinition> {
     Node getContent();
 
     /**
+     * The content node tied to this model exposed as a {@link ContentMap}.
+     * @return
+     */
+    ContentMap getContentMap();
+
+    /**
      * The renderable (paragraph/template) tied to this model.
      */
     RD getDefinition();
@@ -97,7 +104,6 @@ public interface RenderingModel <RD extends RenderableDefinition> {
     /**
      * The top root model of the rendering process.
      * In for example the scope of rendering a page this would be the pages model.
-     *
      */
     RenderingModel<?> getRoot();
 

magnolia-rendering/src/main/java/info/magnolia/rendering/model/RenderingModelImpl.java
@@ -35,6 +35,7 @@ package info.magnolia.rendering.model;
 
 import javax.jcr.Node;
 
+import info.magnolia.jcr.util.ContentMap;
 import info.magnolia.rendering.template.RenderableDefinition;
 
 
@@ -72,6 +73,11 @@ public class RenderingModelImpl<RD extends RenderableDefinition> implements Rend
     @Override
     public Node getContent() {
         return this.content;
+    }
+
+    @Override
+    public ContentMap getContentMap() {
+        return new ContentMap(content);
     }
 
     /**

magnolia-rendering/src/test/java/info/magnolia/rendering/model/RenderingModelImplTest.java
@@ -35,6 +35,7 @@ package info.magnolia.rendering.model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import info.magnolia.jcr.util.ContentMap;
 import info.magnolia.rendering.template.configured.ConfiguredRenderableDefinition;
 import info.magnolia.test.mock.jcr.MockNode;
 
@@ -51,13 +52,26 @@ public class RenderingModelImplTest {
     public void testGetContent() {
         // GIVEN
         MockNode content = new MockNode();
-        RenderingModelImpl model = new RenderingModelImpl(content, null,null);
+        RenderingModelImpl<ConfiguredRenderableDefinition> model = new RenderingModelImpl<ConfiguredRenderableDefinition>(content, null,null);
 
         // WHEN
         Node result = model.getContent();
 
         // THEN
         assertEquals(content, result);
+    }
+
+    @Test
+    public void testGetContentMap() {
+        // GIVEN
+        MockNode content = new MockNode();
+        RenderingModelImpl<ConfiguredRenderableDefinition> model = new RenderingModelImpl<ConfiguredRenderableDefinition>(content, null,null);
+
+        // WHEN
+        ContentMap result = model.getContentMap();
+
+        // THEN
+        assertEquals(content, result.getJCRNode());
     }
 
     @Test
@@ -93,7 +107,7 @@ public class RenderingModelImplTest {
         RenderingModelImpl<ConfiguredRenderableDefinition> childOfChild = new RenderingModelImpl<ConfiguredRenderableDefinition>(null, null, child);
 
         // WHEN
-        RenderingModel result = childOfChild.getRoot();
+        RenderingModel<?> result = childOfChild.getRoot();
 
         // THEN
         assertEquals(parent, result);

richardbigzhou/magnolia-cms_ce

commit 0564993cf1fa59bdfc753d446af1a1e986775209

fixed compilation error introduced with r50243 by adding implementation for newly added abstract method in RenderingModel
git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/community/bundle/trunk@50278 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

magnolia-integration-tests/magnolia-integration-tests-framework/src/main/java/info/magnolia/testframework/util/ComponentModelForTests.java
@@ -34,6 +34,7 @@
 package info.magnolia.testframework.util;
 
 import info.magnolia.cms.core.Content;
+import info.magnolia.jcr.util.ContentMap;
 import info.magnolia.rendering.template.RenderableDefinition;
 import info.magnolia.rendering.model.RenderingModel;
 import java.util.Arrays;
@@ -83,6 +84,11 @@ public class ComponentModelForTests implements RenderingModel {
     }
 
     public RenderingModel<?> getRoot() {
+        return null;
+    }
+
+    public ContentMap getContentMap() {
+        // TODO Auto-generated method stub
         return null;
     }
 }