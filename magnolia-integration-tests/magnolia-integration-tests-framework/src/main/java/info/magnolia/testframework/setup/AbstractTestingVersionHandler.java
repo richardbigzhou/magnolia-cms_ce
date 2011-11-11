c6ab7c698d70ac6c41d8fe2e2a3433c3675c2b08
Daniel Lipp <daniel.lipp@magnolia-cms.com>
2011-11-11 15:13:18 +0000

    QA: do no longer use deprecated constant
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/community/bundle/trunk@51384 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-integration-tests/magnolia-integration-tests-framework/src/main/java/info/magnolia/testframework/setup/AbstractTestingVersionHandler.java
+++ magnolia-integration-tests/magnolia-integration-tests-framework/src/main/java/info/magnolia/testframework/setup/AbstractTestingVersionHandler.java
@@ -33,7 +33,6 @@
  */
 package info.magnolia.testframework.setup;
 
-import info.magnolia.cms.beans.config.ContentRepository;
 import info.magnolia.module.AbstractModuleVersionHandler;
 import info.magnolia.module.InstallContext;
 import info.magnolia.module.delta.ArrayDelegateTask;
@@ -48,6 +47,7 @@ import info.magnolia.module.model.Version;
 import info.magnolia.nodebuilder.task.ErrorHandling;
 import info.magnolia.nodebuilder.task.ModuleNodeBuilderTask;
 import info.magnolia.nodebuilder.task.NodeBuilderTask;
+import info.magnolia.repository.RepositoryConstants;
 import info.magnolia.testframework.htmlunit.AbstractMagnoliaIntegrationTest;
 
 import java.util.ArrayList;
@@ -92,7 +92,7 @@ public abstract class AbstractTestingVersionHandler extends AbstractModuleVersio
         list.add(new PropertiesImportTask("Test config content", "Imports content in the config workspace", "config", getConfigImportPropertiesFile()));
         list.add(new PropertiesImportTask("Test website content", "Imports content in the website workspace", "website", getWebsiteImportPropertiesFile()));
 
-        list.add(new CheckAndModifyPropertyValueTask("Activation", "Changes public URL", ContentRepository.CONFIG,
+        list.add(new CheckAndModifyPropertyValueTask("Activation", "Changes public URL", RepositoryConstants.CONFIG,
                 "/server/activation/subscribers/magnoliaPublic8080", "URL", "http://localhost:8080/magnoliaPublic", AbstractMagnoliaIntegrationTest.Instance.PUBLIC.getURL()));
 
         return list;
