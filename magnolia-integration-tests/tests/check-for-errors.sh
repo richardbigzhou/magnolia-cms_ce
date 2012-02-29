c90a73f5592503c5b2e2294b13f9d3c815876375
Robert Siska <robert.siska@magnolia-cms.com>
2012-02-29 12:13:20 +0000

    Exit codes returning fixed.
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/community/bundle/trunk@55187 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-integration-tests/tests/check-for-errors.sh
+++ magnolia-integration-tests/tests/check-for-errors.sh
@@ -4,7 +4,7 @@ MAGNOLIA_URL="http://localhost:8088/magnoliaTest"
 USER=superuser
 PASS=superuser
 
-function CheckForErrors {
+CheckForErrors() {
     LOGIN=$1
     
     TMPDIR='/tmp/magnolia-test-download'
@@ -17,11 +17,8 @@ function CheckForErrors {
     pushd $TMPDIR
 
     echo "Downloading site..." 
-    wget -r -p --keep-session-cookies -e robots=off \
-    $MAGNOLIA_URL/demo-features.html$LOGIN \
-    $MAGNOLIA_URL/demo-project.html$LOGIN \
-    $MAGNOLIA_URL/ftl-sample-site.html$LOGIN \
-    $MAGNOLIA_URL/jsp-sample-site.html$LOGIN 2>&1 | while read line; do 
+    
+    while read line; do 
         echo $line | grep -q "^--"
 
         if [ $? -eq "0" ]; then
@@ -30,35 +27,40 @@ function CheckForErrors {
 
         echo $line | grep -q "HTTP request"
         if [ $? -eq "0" ]; then
-            echo $line | grep -q "40[14]"
+            echo $line | grep -q "40[14]" # 404 Not found, 401 Unauthorized
             if [ $? -eq "0" ]; then
                 echo $url
                 echo $line
                 HTTP_ERROR="1"
             fi
         fi   
-    done
-   
+    done < <(wget -r -p --keep-session-cookies -e robots=off \
+    $MAGNOLIA_URL/demo-features.html$LOGIN \
+    $MAGNOLIA_URL/demo-project.html$LOGIN \
+    $MAGNOLIA_URL/ftl-sample-site.html$LOGIN \
+    $MAGNOLIA_URL/jsp-sample-site.html$LOGIN 2>&1) # process substitution. Not a POSIX way!!!
+       
     echo "Files with FreemarkerError: "
-    find . -type f -iname "*" -print | while read file; do
+    while read file; do
         grep -q --binary-files=without-match -i FreemarkerError "$file"
         if [ $? -eq "0" ]; then
             echo $file
             FREEM_ERROR="1"
         fi
-    done
+    done < <(find $TMPDIR -type f -iname "*" -print)
     
     popd
 
     rm -Rf $TMPDIR
 }
 
+
 CheckForErrors ?mgnlUserId=$USER\&mgnlUserPSWD=$PASS
 
-if [ $HTTP_ERROR -lt "0" ]; then
+if [ $HTTP_ERROR -gt "0" ]; then
     exit 1
 fi
 
-if [ $FREEM_ERROR -lt "0" ]; then
+if [ $FREEM_ERROR -gt "0" ]; then
     exit 2
 fi
