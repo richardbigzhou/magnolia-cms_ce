4a523a5d1851bd35b209c1423b735946d6e7ebea
Robert Siska <robert.siska@magnolia-cms.com>
2012-03-02 12:00:25 +0000

    Workaround for logging out problem
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/community/bundle/trunk@55486 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-integration-tests/tests/src/main/check-for-errors.sh
+++ magnolia-integration-tests/tests/src/main/check-for-errors.sh
@@ -2,6 +2,7 @@
 
 CheckForErrors() {
     URL=$1
+    AUTH=$2
     
     TMPDIR='/tmp/magnolia-test-download'
     rm -Rf $TMPDIR
@@ -12,23 +13,37 @@ CheckForErrors() {
 
     pushd $TMPDIR
 
-    echo "--------------"
-    echo "Downloading $1"
-    echo "--------------"
+    echo "-------------------------"
+    echo "Downloading ${URL}${AUTH}"
+    echo "-------------------------"
     
     HTTP_ERROR=$(
     wget -r -p -U Mozilla -e robots=off \
-         --keep-session-cookies \
-    $URL 2>&1 | { while read line; do 
+        --keep-session-cookies \
+    ${URL}${AUTH} 2>&1 | { while read line; do
+        
         echo $line | grep -q "^--"
-
         if [ $? -eq "0" ]; then
-            url=$line
+            url=$(echo $line | sed 's/.*\(http.*\)$/\1/')
         fi
 
         echo $line | grep -q "HTTP request"
         if [ $? -eq "0" ]; then
-            echo $line | grep -q "\(404\|401\|403\)" # 404 Not found, 401 Unauthorized, 403 Forbidden
+             
+            echo $line | grep -q "403" #if we get 403, try to download file with login query
+            if [ $? -eq "0" ]; then
+                wget ${url}${AUTH} 2>&1 | grep -q "HTTP.*403"
+                if [ $? -gt "0" ]; then
+                    line=""
+                fi
+                echo ${url} | grep -q "mgnlLogout"
+                if [ $? -eq "0" ]; then
+                    line=""
+                fi
+            fi  # if only wget`s --reject options would filter url requests, not just filenames :(
+            
+             
+            echo $line | grep -q "\(404\|401\|403\)" # 404 Not found, 401 Unauthorized
             if [ $? -eq "0" ]; then
                 echo >&2 $url
                 echo >&2 $line
@@ -55,20 +70,22 @@ CheckForErrors() {
     rm -Rf $TMPDIR
 }
 
+HOST="http://localhost:8088"
+
 while [ $# -gt 0 ]; do # Loop through all arguments
     PAGE=$1
     shift
 
-    ROOTURL="http://localhost:8088/magnoliaTestPublic"
+    ROOTURL="$HOST/magnoliaTestPublic"
     USER=superuser
     PASS=superuser
 
     CheckForErrors $ROOTURL/$PAGE
-    CheckForErrors $ROOTURL/$PAGE?mgnlUserId=$USER\&mgnlUserPSWD=$PASS
+    CheckForErrors $ROOTURL/$PAGE ?mgnlUserId=$USER\&mgnlUserPSWD=$PASS
 
-    ROOTURL="http://localhost:8088/magnoliaTest"
+    ROOTURL="$HOST/magnoliaTest"
 
-    CheckForErrors $ROOTURL/$PAGE?mgnlUserId=$USER\&mgnlUserPSWD=$PASS
+    CheckForErrors $ROOTURL/$PAGE ?mgnlUserId=$USER\&mgnlUserPSWD=$PASS
 done
 
 if [ $HTTP_ERROR -gt "0" ]; then
