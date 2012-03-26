4ebd47b2b8c0729ba658299e314ff7fb9ab9c360
Federico Grilli <federico.grilli@magnolia-cms.com>
2012-03-26 15:21:33 +0000

    MAGNOLIA-4138: created a selenium profile. Ignored tests as long as we do not find a way not to execute them when we're on a display-less machine
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/community/bundle/branches/magnolia-bundle-4.5-with-selenium@56238 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/PageEditorTest.java
+++ magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/PageEditorTest.java
@@ -39,6 +39,7 @@ import info.magnolia.testframework.htmlunit.AbstractMagnoliaIntegrationTest;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.thoughtworks.selenium.DefaultSelenium;
@@ -48,6 +49,7 @@ import com.thoughtworks.selenium.DefaultSelenium;
  * @version $Id$
  *
  */
+@Ignore("We need to find a way not to execute these tests on display-less machines. See MAGNOLIA-4138")
 public class PageEditorTest extends AbstractMagnoliaIntegrationTest {
     private static DefaultSelenium selenium;
     /**
