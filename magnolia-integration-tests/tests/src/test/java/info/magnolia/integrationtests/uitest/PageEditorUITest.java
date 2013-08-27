deeca870027324b9b973a7e11269f23e889d7a16
fgrilli <federico.grilli@magnolia-cms.com>
2013-08-27 10:57:43 +0200

    MGNLUI-1955 fix test by switching from page editor iframe back to default content

--- magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/PageEditorUITest.java
+++ magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/PageEditorUITest.java
@@ -142,6 +142,7 @@ public class PageEditorUITest extends AbstractMagnoliaUITest {
 
         // THEN
         assertTrue("Edit bars should be around.", isExisting(getElementByPath(By.cssSelector("div.mgnlEditorBar"))));
+        switchToDefaultContent();
         assertTrue("We should still be in edit mode.", driver.getCurrentUrl().contains("about:edit"));
     }
 
