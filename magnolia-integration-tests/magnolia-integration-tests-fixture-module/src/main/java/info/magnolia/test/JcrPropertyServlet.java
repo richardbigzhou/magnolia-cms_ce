377adc20348456940b3ebf40940b8c5dcc426e8e
Philip Mundt <philip.mundt@magnolia-cms.com>
2013-08-28 09:20:05 +0200

    Adding JcrPropertyServlet to magnolia-integration-tests-fixture-module.
    - Servlet enables "querying" of JCR properties fir e.g. verification in tests
    - Adding test that checks whether module was properly installed

---
+++ magnolia-integration-tests/magnolia-integration-tests-fixture-module/src/main/java/info/magnolia/test/JcrPropertyServlet.java
@@ -0,0 +1,75 @@
+/**
+ * This file Copyright (c) 2013 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.test;
+
+import info.magnolia.context.MgnlContext;
+import info.magnolia.repository.RepositoryConstants;
+
+import java.io.IOException;
+
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Servlet that can read properties from JCR to verify settings in tests.
+ */
+public class JcrPropertyServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(JcrPropertyServlet.class);
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final String path = request.getParameter("path");
+        final String workspace = StringUtils.defaultString(request.getParameter("workspace"), RepositoryConstants.CONFIG);
+
+        try {
+            Property property = MgnlContext.getJCRSession(workspace).getProperty(path);
+
+            response.getWriter().write(property.getString());
+        } catch (RepositoryException e) {
+            log.warn("Could not read property [{}] from workspace [{}]", new Object[] {path, workspace});
+            throw new ServletException("Could not read property [" + path + "]");
+        }
+    }
+
+}
+
