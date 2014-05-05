cffd7a3257b32b05f2fb08315741efdd7b290852
Christopher Zimmermann <christopher.zimmermann@magnolia-cms.com>
2014-05-05 13:42:30 +0200

    MGNLUI-2817 KeyboardShortcutUITest- check on email of contact instead of name because name uses unreliable column generator.

--- magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/KeyboardShortcutUITest.java
+++ magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/KeyboardShortcutUITest.java
@@ -318,27 +318,29 @@ public class KeyboardShortcutUITest extends AbstractMagnoliaUITest {
         final String nameFirst = "Joe";
         final String nameLast = "Testkeyboard";
         String contactName = nameFirst + " " + nameLast;
+        String email = nameFirst + "@" + nameLast + ".com";
+
 
         getAppIcon("Contacts").click();
         assertAppOpen("Contacts");
         getActionBarItem("Add contact").click();
 
-        fillInRequiredContactFields(nameFirst, nameLast);
+        fillInRequiredContactFields(nameFirst, nameLast, email);
 
         // WHEN
         getFormTextField("Country").sendKeys(ENTER_OR_RETURN_KEY);
-        delay(4, "Long pause to ensure that name renders as fullname, not just 'jtestkeyboard'");
+        delay(2, "");
 
         // THEN
         //Check that editor is closed
         WebElement editorTab = getTabForCaption(contactName);
         assertFalse("ENTER key should have closed the DetailEditor subapp.", isExisting(editorTab));
         //Check that entry is added.
-        WebElement newRow = getTreeTableItem(contactName);
+        WebElement newRow = getTreeTableItem(email);
         assertTrue("ENTER key should have caused new contact to be created, but no new contact is present.", isExisting(newRow));
 
         // Cleanup - delete the created contact.
-        deleteTreeTableRow("Delete contact", contactName);
+        deleteTreeTableRow("Delete contact", email);
     }
 
     /**
@@ -351,12 +353,13 @@ public class KeyboardShortcutUITest extends AbstractMagnoliaUITest {
         // GIVEN
         final String nameFirst = "Joe2";
         final String nameLast = "Testkeyboard";
+        String email = nameFirst + "@" + nameLast + ".com";
 
         getAppIcon("Contacts").click();
         assertAppOpen("Contacts");
         getActionBarItem("Add contact").click();
 
-        fillInRequiredContactFields(nameFirst, nameLast);
+        fillInRequiredContactFields(nameFirst, nameLast, email);
 
         // WHEN
         getFormTextAreaField("Street address").sendKeys(ENTER_OR_RETURN_KEY);
@@ -374,12 +377,12 @@ public class KeyboardShortcutUITest extends AbstractMagnoliaUITest {
      * @param nameFirst
      * @param nameLast
      */
-    private void fillInRequiredContactFields(String nameFirst, String nameLast){
+    private void fillInRequiredContactFields(String nameFirst, String nameLast, String email){
         getTabForCaption("Personal").click();
         setFormTextFieldText("First name", nameFirst);
         setFormTextFieldText("Last name", nameLast);
         getTabForCaption("Contact details").click();
-        setFormTextFieldText("E-Mail address", "test@test.com");
+        setFormTextFieldText("E-Mail address", email);
         getTabForCaption("Address").click();
         setFormTextFieldText("Organization", "org");
         setFormTextAreFieldText("Street address", "address-125");
