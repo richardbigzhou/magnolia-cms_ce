ee50b4d4617ef1e1d187bceff6152181a80c4bfd
Christoph Meier <christoph.meier@webmover.ch>
2014-05-14 21:53:48 +0200

    MGNLUI-2599 UI-Test for: Enabling simultaneously creation of favorite and favoriteGroup and adding the former as child of the latter.

--- magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/FavoriteUITest.java
+++ magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/FavoriteUITest.java
@@ -35,10 +35,12 @@ package info.magnolia.integrationtests.uitest;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 /**
@@ -46,6 +48,50 @@ import org.openqa.selenium.WebElement;
  * Should be rewritten/expanded as soon as MGNLUI-1189 is fixed - right now it can easily fail e.g. if for any reason there's a several favorites and hence several remove buttons.
  */
 public class FavoriteUITest extends AbstractMagnoliaUITest {
+
+
+    @Test
+    public void addFavoriteAndGroupSimultaneously() {
+        // GIVEN
+        //
+        getAppIcon("Pages").click();
+        getTreeTableItem("ftl-sample-site").click();
+        //getActionBarItem("Edit page").click();
+        delay("Give some time to open the page");
+        getShellAppIcon("icon-favorites").click();
+        delay("Give some time to fill in values from previous location.");
+
+        // WHEN
+        //
+        getButton("dialog-header", "Add new").click();
+        delay("Give some time to open the favorites dialog.");
+        String newGroupName = String.valueOf((new Date()).getTime());
+        getElementByPath(By.xpath("//*[contains(@class, 'v-filterselect')]/*[@class = 'v-filterselect-input']")).sendKeys(newGroupName);
+        simulateKeyPress(Keys.TAB);
+        getButton("v-button-commit", "Add").click();
+        delay("Wait again ...");
+
+        // THEN
+        WebElement newGroupElement = null;
+        List<WebElement> groupTitles = getElementsByXPath("//div[contains(@class, 'favorites-group-title')]/*[contains(@class, 'v-textfield')]");
+        for (WebElement element : groupTitles) {
+            if (newGroupName.equals(element.getAttribute("value"))) {
+                newGroupElement = element;
+                break;
+            }
+        }
+        assertNotNull(newGroupElement);
+        WebElement newFavoriteElement = newGroupElement.findElement(By.xpath("//*[contains(concat(' ', @class, ' '), ' favorites-entry ')]"));
+        assertNotNull(newFavoriteElement);
+
+
+        // let's delete the group finally (which makes it easier for upcoming tests)
+        newGroupElement.click();
+        getElementByPath(By.xpath("//*[@class = 'icon-trash']")).click();
+        delay("Wait for Confirmation Dialog.");
+        getDialogConfirmButton().click();
+        delay("Remove is not always super fast...");
+    }
 
     @Test
     public void addAndRemoveFavorite() {
@@ -113,5 +159,17 @@ public class FavoriteUITest extends AbstractMagnoliaUITest {
         List<WebElement> selected = getElementsByPath(By.cssSelector(".favorites-entry.selected"), 1);
         assertNotNull("We expect one selected favourite", selected);
         assertEquals(1, selected.size());
+        // at the end, removing the the added stuff (which makes it easier for upcoming tests)
+        // 1st one is already selected
+        getElementByPath(By.xpath("//*[@class = 'icon-trash']")).click();
+        delay("Wait for Confirmation Dialog.");
+        getDialogConfirmButton().click();
+        delay("Remove is not always super fast...");
+        // 2nd one easy to fetch
+        getElementByPath(By.cssSelector(".favorites-entry .icon")).click();
+        getElementByPath(By.xpath("//*[@class = 'icon-trash']")).click();
+        delay("Wait for Confirmation Dialog.");
+        getDialogConfirmButton().click();
+        delay("Remove is not always super fast...");
     }
 }
