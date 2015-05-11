48ab3199576a3c29acb0f043cc5b1f9bb017b052
Evzen Fochr <evzen.fochr@magnolia-cms.com>
2015-05-11 14:23:43 +0200

    MAGNOLIA-6205 merge ConfigAppUITest.java and ConfigurationAppUITest.java

--- magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/ConfigurationAppUITest.java
+++ magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/ConfigurationAppUITest.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2014-2015 Magnolia International
+ * This file Copyright (c) 2013-2015 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
@@ -36,13 +36,101 @@ package info.magnolia.integrationtests.uitest;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * UI Tests for the configuration app.
  */
 public class ConfigurationAppUITest extends AbstractMagnoliaUITest {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfigurationAppUITest.class);
     public static final String CONFIGURATION_APP = "Configuration";
+
+    /**
+     * This test checks if properties are properly escaped in the ConfigurationApp.
+     *
+     * @see <a href="http://jira.magnolia-cms.com/browse/MGNLUI-2151">MGNLUI-2151</a>
+     */
+    @Test
+    public void testIfPropertiesAreEscaped() {
+        // GIVEN
+        final String unEscapedHTML = "<img src=\"http://hudson.magnolia-cms.com/static/6629b508/images/32x32/health-00to19.gif\" width=\"32\" height=\"32\" onmouseover='alert(\"Pwwned\")' />";
+        final String folderName = "testFolder";
+        final String nodeName = "item";
+        final String propertyName = "property";
+
+        // WHEN
+        // Go to pages App
+        getAppIcon(CONFIGURATION_APP).click();
+        assertAppOpen(CONFIGURATION_APP);
+
+        deleteUntitledFolderIfExisting();
+
+        // Create folder and rename it
+        getEnabledActionBarItem("Add folder").click();
+        getTreeTableItem("untitled").click();
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", folderName);
+
+        getDialogCommitButton().click();
+
+        // Create content node and rename it
+        getEnabledActionBarItem("Add content node").click();
+        getTreeTableItem("untitled").click();
+        delay(1, "Wait a second for actionbar to update");
+
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", nodeName);
+
+        getDialogCommitButton().click();
+
+        // Create property and set name & value
+        getEnabledActionBarItem("Add property").click();
+        getTreeTableItem("untitled").click();
+        delay(1, "Wait a second for actionbar to update");
+
+        getEnabledActionBarItem("Edit property").click();
+        setFormTextFieldText("Name", propertyName);
+        setFormTextFieldText("Value", unEscapedHTML);
+
+        getDialogCommitButton().click();
+
+        delay(2, "Wait a second for the image to show up");
+
+        // THEN
+        // Unescaped property shouldn't show up
+        WebElement propertyUnescaped = getElementByPath(By.xpath("//div[@class='v-table-cell-wrapper']//img[contains(@src, 'gif')]"));
+        if (isExisting(propertyUnescaped)) {
+            log.info("Found un-escaped property [{}]", propertyUnescaped);
+
+            Actions mouseOverActionUnescaped = getDriverActions();
+            mouseOverActionUnescaped.moveToElement(propertyUnescaped).build().perform();
+
+            delay("Wait for alert to appear");
+
+            assertFalse("We expect to see no alert.", isAlertPresent());
+        }
+
+        // Escaped property shouldn't trigger alert
+        WebElement property = getElementByPath(By.xpath("//div[@class='v-table-cell-wrapper' and contains(text(), 'onmouseover')]"));
+        if (!(property instanceof NonExistingWebElement)) {
+            log.info("Found escaped property [{}]", property);
+
+            Actions mouseOverAction = getDriverActions();
+            mouseOverAction.moveToElement(property).build().perform();
+
+            delay("Wait for alert to appear");
+
+            assertFalse("We expect to see no alert.", isAlertPresent());
+        }
+    }
 
     @Test
     public void canPublishUnpublishAndDeleteNewNode() {
@@ -74,5 +162,24 @@ public class ConfigurationAppUITest extends AbstractMagnoliaUITest {
         delay("Delete might take some time");
         refreshTreeView();
         assertFalse("Untitled should be gone", isExisting(getTreeTableItem("untitled")));
+    }
+
+    @Test
+    public void canPublishCannotUnpublishLevel1Or2Node() {
+
+    }
+
+    private void deleteUntitledFolderIfExisting() {
+        WebElement untitledFolder = getTreeTableItem("untitled");
+        if (isExisting(untitledFolder)) {
+            untitledFolder.click();
+            getEnabledActionBarItem("Delete item").click();
+            getDialogConfirmButton().click();
+            delay("Delete might take some time");
+            refreshTreeView();
+
+            List<WebElement> rows = getElementsByPath(By.xpath(String.format("//*[contains(@class, 'v-table-cell-wrapper') and text() = '%s']", "untitled")), 0);
+            assertTrue(rows.isEmpty());
+        }
     }
 }
