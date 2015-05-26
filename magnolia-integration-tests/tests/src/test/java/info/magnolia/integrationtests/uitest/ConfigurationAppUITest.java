c1186e7fc2e532083f7506570860f50d36ee3032
Evzen Fochr <evzen.fochr@magnolia-cms.com>
2015-05-26 18:32:19 +0200

    MAGNOLIA-6205 Merge ConfigAppUITest and ConfigurationAppUITest - skip testing unpublication of lvl1 + 2 nodes - test availability of unpublication action (~MGNLUI-3426)

--- magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/ConfigurationAppUITest.java
+++ magnolia-integration-tests/tests/src/test/java/info/magnolia/integrationtests/uitest/ConfigurationAppUITest.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2014-2015 Magnolia International
+ * This file Copyright (c) 2013-2015 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
@@ -35,35 +35,146 @@ package info.magnolia.integrationtests.uitest;
 
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+import java.util.List;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * UI Tests for the configuration app.
  */
 public class ConfigurationAppUITest extends AbstractMagnoliaUITest {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfigurationAppUITest.class);
     public static final String CONFIGURATION_APP = "Configuration";
+
+    /**
+     * This test checks if properties are properly escaped in the ConfigurationApp.
+     *
+     * @see <a href="http://jira.magnolia-cms.com/browse/MGNLUI-2151">MGNLUI-2151</a>
+     */
+    @Test
+    public void testIfPropertiesAreEscaped() {
+        // GIVEN
+        final String unEscapedHTML = "<img src=\"http://hudson.magnolia-cms.com/static/6629b508/images/32x32/health-00to19.gif\" width=\"32\" height=\"32\" onmouseover='alert(\"Pwwned\")' />";
+        final String folderName = "testFolder";
+        final String nodeName = "item";
+        final String propertyName = "property";
+
+        // WHEN
+        // Go to pages App
+        getAppIcon(CONFIGURATION_APP).click();
+        assertAppOpen(CONFIGURATION_APP);
+
+        deleteUntitledFolderIfExisting();
+
+        // Create folder and rename it
+        getEnabledActionBarItem("Add folder").click();
+        getTreeTableItem("untitled").click();
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", folderName);
+
+        getDialogCommitButton().click();
+
+        // Create content node and rename it
+        getEnabledActionBarItem("Add content node").click();
+        getTreeTableItem("untitled").click();
+        waitUntil(elementToBeClickable(getEnabledActionBarItem("Rename item")));
+
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", nodeName);
+
+        getDialogCommitButton().click();
+
+        // Create property and set name & value
+        getEnabledActionBarItem("Add property").click();
+        getTreeTableItem("untitled").click();
+        waitUntil(elementToBeClickable(getEnabledActionBarItem("Edit property")));
+
+        getEnabledActionBarItem("Edit property").click();
+        setFormTextFieldText("Name", propertyName);
+        setFormTextFieldText("Value", unEscapedHTML);
+
+        getDialogCommitButton().click();
+
+        delay(2, "Wait a second for the image to show up");
+
+        // THEN
+        // Unescaped property shouldn't show up
+        WebElement propertyUnescaped = getElementByPath(By.xpath("//div[@class='v-table-cell-wrapper']//img[contains(@src, 'gif')]"));
+        if (isExisting(propertyUnescaped)) {
+            log.info("Found un-escaped property [{}]", propertyUnescaped);
+
+            Actions mouseOverActionUnescaped = getDriverActions();
+            mouseOverActionUnescaped.moveToElement(propertyUnescaped).build().perform();
+
+            delay("Wait for alert to appear");
+
+            assertFalse("We expect to see no alert.", isAlertPresent());
+        }
+
+        // Escaped property shouldn't trigger alert
+        WebElement property = getElementByPath(By.xpath("//div[@class='v-table-cell-wrapper' and contains(text(), 'onmouseover')]"));
+        if (!(property instanceof NonExistingWebElement)) {
+            log.info("Found escaped property [{}]", property);
+
+            Actions mouseOverAction = getDriverActions();
+            mouseOverAction.moveToElement(property).build().perform();
+
+            delay("Wait for alert to appear");
+
+            assertFalse("We expect to see no alert.", isAlertPresent());
+        }
+    }
 
     @Test
     public void canPublishUnpublishAndDeleteNewNode() {
         getAppIcon(CONFIGURATION_APP).click();
         assertAppOpen(CONFIGURATION_APP);
-        waitUntil(appIsLoaded());
 
         getEnabledActionBarItem("Add folder").click();
 
         getTreeTableItem("untitled").click();
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", "lvl1");
+        waitUntil(textToBePresentInElementValue(getFormTextField("Name"), "lvl1"));
+        getDialogCommitButton().click();
+
+        getEnabledActionBarItem("Add folder").click();
+
+        getTreeTableItem("untitled").click();
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", "lvl2");
+        waitUntil(textToBePresentInElementValue(getFormTextField("Name"), "lvl2"));
+        getDialogCommitButton().click();
+
+        getEnabledActionBarItem("Add folder").click();
+
+        getTreeTableItem("untitled").click();
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", "lvl3");
+        waitUntil(textToBePresentInElementValue(getFormTextField("Name"), "lvl3"));
+        getDialogCommitButton().click();
 
         getEnabledActionBarItem("Add content node").click();
         getEnabledActionBarItem("Add property").click();
 
-        // publish
+        // publish lvl1
+        refreshTreeView();
+        getTreeTableItem("lvl1").click();
         getEnabledActionBarItem("Publish incl. subnodes").click();
         delay(5, "Publication may take some time");
         refreshTreeView();
         assertThat(getSelectedActivationStatusIcon().getAttribute("class"), containsString(COLOR_GREEN_ICON_STYLE));
 
-        // unpublish
+        // unpublish lvl3
+        getTreeTableItem("lvl3").click();
         getEnabledActionBarItem("Unpublish").click();
         refreshTreeView();
         assertThat(getSelectedActivationStatusIcon().getAttribute("class"), containsString(COLOR_RED_ICON_STYLE));
@@ -73,6 +184,69 @@ public class ConfigurationAppUITest extends AbstractMagnoliaUITest {
         getDialogConfirmButton().click();
         delay("Delete might take some time");
         refreshTreeView();
-        assertFalse("Untitled should be gone", isExisting(getTreeTableItem("untitled")));
+        assertFalse("Lvl3 folder should be gone", isExisting(getTreeTableItem("lvl3")));
+    }
+
+    @Test
+    public void unpublishActionIsDisabledForLvl1Or2Nodes() {
+        getAppIcon(CONFIGURATION_APP).click();
+        assertAppOpen(CONFIGURATION_APP);
+
+        getEnabledActionBarItem("Add folder").click();
+
+        getTreeTableItem("untitled").click();
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", "depth1");
+        waitUntil(textToBePresentInElementValue(getFormTextField("Name"), "depth1"));
+        getDialogCommitButton().click();
+
+        getEnabledActionBarItem("Add folder").click();
+
+        getTreeTableItem("untitled").click();
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", "depth2");
+        waitUntil(textToBePresentInElementValue(getFormTextField("Name"), "depth2"));
+        getDialogCommitButton().click();
+
+        getEnabledActionBarItem("Add folder").click();
+
+        getTreeTableItem("untitled").click();
+        getEnabledActionBarItem("Rename item").click();
+        setFormTextFieldText("Name", "depth3");
+        waitUntil(textToBePresentInElementValue(getFormTextField("Name"), "depth3"));
+        getDialogCommitButton().click();
+
+        // publish depth1
+        refreshTreeView();
+        getTreeTableItem("depth1").click();
+        getEnabledActionBarItem("Publish incl. subnodes").click();
+        delay(5, "Publication may take some time");
+        refreshTreeView();
+        assertThat(getSelectedActivationStatusIcon().getAttribute("class"), containsString(COLOR_GREEN_ICON_STYLE));
+
+        // unpublish availability depth1
+        checkDisabledActions("Unpublish");
+
+        // unpublish availability depth2
+        getTreeTableItem("depth2").click();
+        checkDisabledActions("Unpublish");
+
+        // unpublish availability depth3
+        getTreeTableItem("depth3").click();
+        checkEnabledActions("Unpublish");
+    }
+
+    private void deleteUntitledFolderIfExisting() {
+        WebElement untitledFolder = getTreeTableItem("untitled");
+        if (isExisting(untitledFolder)) {
+            untitledFolder.click();
+            getEnabledActionBarItem("Delete item").click();
+            getDialogConfirmButton().click();
+            delay("Delete might take some time");
+            refreshTreeView();
+
+            List<WebElement> rows = getElementsByPath(By.xpath(String.format("//*[contains(@class, 'v-table-cell-wrapper') and text() = '%s']", "untitled")), 0);
+            assertTrue(rows.isEmpty());
+        }
     }
 }
